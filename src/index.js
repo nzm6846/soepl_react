888a866749e6e4160c9db66900dcd616e96e79c6
nzm <nzm6846@gmail.com>
2025-05-17 23:37:21 +0900

    챕터 9 실습
    조건문 이욯으로 원하는 컴포턴트 함수 호출

--- src/index.js
+++ src/index.js
@@ -4,12 +4,22 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+import Library from './chapter_03/Library';
+import Clock from './chapter_04/Clock';
+import CommentList from './chapter_05/CommentList';
+import NotificationList from './chapter_06/NotificationList';
+import Accommodate from './chapter_07/Accommodate';
+import ConfirmButton from './chapter_08/ConfirmButton';
+import ConfirmButtonFeild from './chapter_08/ConfirmButtonFeild';
+import ConfirmButtonFunction from './chapter_08/ConfirmButtonFunction';
+import LandingPage from './chapter_09/LandingPage';
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <LandingPage/>
   </React.StrictMode>
-);
+)
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
