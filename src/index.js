e730da77aec4bc652ec90770cbaced138a847e89
nzm <nzm6846@gmail.com>
2025-05-18 15:50:13 +0900

    챕터10 리스트와 키 실습
    리스트를 이용할 때는 키가 필수 키를 주지않으면 index를 키로 이용해야함

--- src/index.js
+++ src/index.js
@@ -13,11 +13,12 @@ import ConfirmButton from './chapter_08/ConfirmButton';
 import ConfirmButtonFeild from './chapter_08/ConfirmButtonFeild';
 import ConfirmButtonFunction from './chapter_08/ConfirmButtonFunction';
 import LandingPage from './chapter_09/LandingPage';
+import AttendanceBook from './chapter_10/AttendanceBook';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <LandingPage/>
+    <AttendanceBook/>
   </React.StrictMode>
 )
 
